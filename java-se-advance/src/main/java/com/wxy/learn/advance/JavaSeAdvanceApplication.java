c74488962a56c12e4d164388bcf0b79ba7cf7da6
SillyBoy007 <814752288@qq.com>
2020-09-18 16:00:38 +0800

    Java 进阶

---
+++ java-se-advance/src/main/java/com/wxy/learn/advance/JavaSeAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.wxy.learn.advance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSeAdvanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSeAdvanceApplication.class, args);
+    }
+
+}
---
+++ java-se-advance/src/test/java/com/wxy/learn/advance/JavaSeAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxy.learn.advance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSeAdvanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
